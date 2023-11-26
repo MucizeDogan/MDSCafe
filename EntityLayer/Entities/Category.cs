bef6a3541bd12e276a084e197638ed324ae6215e
M-racle <108946729+M-racle@users.noreply.github.com>
2023-11-26 11:09:16 +0300

    Add project files.

---
+++ EntityLayer/Entities/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace EntityLayer.Entities {
+    public class Category {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+    }
+}
