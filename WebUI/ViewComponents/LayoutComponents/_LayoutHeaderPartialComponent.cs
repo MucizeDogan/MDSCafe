0d94a9945e3b3338b96a95b171eeec73e8363e5e
M-racle <108946729+M-racle@users.noreply.github.com>
2023-12-03 12:38:08 +0300

    Layout and header Partial Component was created for UI

---
+++ WebUI/ViewComponents/LayoutComponents/_LayoutHeaderPartialComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.LayoutComponents {
+    public class _LayoutHeaderPartialComponent : ViewComponent{
+        
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
