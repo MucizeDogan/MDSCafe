73191e1f9f15660790a2fd67645f4de31bb09ec7
M-racle <108946729+M-racle@users.noreply.github.com>
2023-12-03 13:49:38 +0300

    Partial Components have been completed and the main layout has been made more functional with less code.

---
+++ WebUI/ViewComponents/LayoutComponents/_LayoutSideBarPartialComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.LayoutComponents {
+    public class _LayoutSideBarPartialComponent : ViewComponent{
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
