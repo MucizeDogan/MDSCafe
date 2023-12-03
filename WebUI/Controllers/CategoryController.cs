fe24d9df53600e02f35efae639c928ad47073ecb
M-racle <108946729+M-racle@users.noreply.github.com>
2023-12-03 16:52:50 +0300

    I consumed and listed the category list on the UI side.

--- WebUI/Controllers/CategoryController.cs
+++ WebUI/Controllers/CategoryController.cs
@@ -1,8 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.Dtos.CategoryDto;
 
 namespace WebUI.Controllers {
     public class CategoryController : Controller {
-        public IActionResult Index() {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IActionResult >Index() {
+            var client = _httpClientFactory.CreateClient(); // Bir istemci oluşturdum.
+            var res = await client.GetAsync("https://localhost:7052/api/Category"); // İstekte bulunacağımız apinin url sini yazıyoruz
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync(); // json dan gelen içerği string formatta oku
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData); // Json datayı çözüp normal metine dönüştürür(DeserializeObject)
+                return View(values);
+            }
             return View();
         }
     }
---
+++ WebUI/Dtos/CategoryDto/ResultCategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebUI.Dtos.CategoryDto {
+    public class ResultCategoryDto {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+    }
+}
