445e719a77125a5ba95f4949c10972c1a137cd64
M-racle <108946729+M-racle@users.noreply.github.com>
2023-12-08 15:37:10 +0300

    Dtos were created for the Featured and Social Media sections. Controllers and Views were created and tested for both.

---
+++ WebUI/Controllers/FeatureController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using WebUI.Dtos.FeatureDto;
+
+namespace WebUI.Controllers {
+    public class FeatureController : Controller {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FeatureController(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index() {
+            var client = _httpClientFactory.CreateClient(); // Bir istemci oluşturdum.
+            var res = await client.GetAsync("https://localhost:7052/api/Feature"); // İstekte bulunacağımız apinin url sini yazıyoruz
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync(); // json dan gelen içerği string formatta oku
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData); // Json datayı çözüp normal metine dönüştürür(DeserializeObject)
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateFeature() {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(CreateFeatureDto createFeatureDto) {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createFeatureDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var res = await client.PostAsync("https://localhost:7052/api/Feature", stringContent);
+            if (res.IsSuccessStatusCode) {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteFeature(int id) {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.DeleteAsync($"https://localhost:7052/api/Feature/{id}");
+            if (res.IsSuccessStatusCode) {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateFeature(int id) {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.GetAsync($"https://localhost:7052/api/Feature/{id}");
+            if (!res.IsSuccessStatusCode) {
+                return View();
+            }
+            var jsonData = await res.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<UpdateFeatureDto>(jsonData);
+            return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureDto updateFeatureDto) {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateFeatureDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var res = await client.PutAsync("https://localhost:7052/api/Feature/", stringContent);
+            if (!res.IsSuccessStatusCode) {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebUI/Controllers/SocialMediaController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using WebUI.Dtos.SocialMediaDto;
+
+namespace WebUI.Controllers {
+    public class SocialMediaController : Controller {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SocialMediaController(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index() {
+            var client = _httpClientFactory.CreateClient(); // Bir istemci oluşturdum.
+            var res = await client.GetAsync("https://localhost:7052/api/SocialMedia"); // İstekte bulunacağımız apinin url sini yazıyoruz
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync(); // json dan gelen içerği string formatta oku
+                var values = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData); // Json datayı çözüp normal metine dönüştürür(DeserializeObject)
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateSocialMedia() {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto) {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSocialMediaDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var res = await client.PostAsync("https://localhost:7052/api/SocialMedia", stringContent);
+            if (res.IsSuccessStatusCode) {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteSocialMedia(int id) {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.DeleteAsync($"https://localhost:7052/api/SocialMedia/{id}");
+            if (res.IsSuccessStatusCode) {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateSocialMedia(int id) {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.GetAsync($"https://localhost:7052/api/SocialMedia/{id}");
+            if (!res.IsSuccessStatusCode) {
+                return View();
+            }
+            var jsonData = await res.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<UpdateSocialMediaDto>(jsonData);
+            return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto) {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateSocialMediaDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var res = await client.PutAsync("https://localhost:7052/api/SocialMedia/", stringContent);
+            if (!res.IsSuccessStatusCode) {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
