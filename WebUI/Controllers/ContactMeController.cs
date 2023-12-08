3bc42f16b1afbf2c5a9f1cc6044c19cf190e7828
M-racle <108946729+M-racle@users.noreply.github.com>
2023-12-08 14:14:27 +0300

    DTOs have been created for the contact field. Controllers and views were created and tested.

---
+++ WebUI/Controllers/ContactMeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using WebUI.Dtos.ContactMeDto;
+
+namespace WebUI.Controllers {
+    public class ContactMeController : Controller {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactMeController(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index() {
+            var client = _httpClientFactory.CreateClient(); // Bir istemci oluşturdum.
+            var res = await client.GetAsync("https://localhost:7052/api/ContactMe"); // İstekte bulunacağımız apinin url sini yazıyoruz
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync(); // json dan gelen içerği string formatta oku
+                var values = JsonConvert.DeserializeObject<List<ResultContactMeDto>>(jsonData); // Json datayı çözüp normal metine dönüştürür(DeserializeObject)
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateContactMe() {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateContactMe(CreateContactMeDto createContactMeDto) {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactMeDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var res = await client.PostAsync("https://localhost:7052/api/ContactMe", stringContent);
+            if (res.IsSuccessStatusCode) {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteContactMe(int id) {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.DeleteAsync($"https://localhost:7052/api/ContactMe/{id}");
+            if (res.IsSuccessStatusCode) {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateContactMe(int id) {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.GetAsync($"https://localhost:7052/api/ContactMe/{id}");
+            if (!res.IsSuccessStatusCode) {
+                return View();
+            }
+            var jsonData = await res.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<UpdateContactMeDto>(jsonData);
+            return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateContactMe(UpdateContactMeDto updateContactMeDto) {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateContactMeDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var res = await client.PutAsync("https://localhost:7052/api/ContactMe/", stringContent);
+            if (!res.IsSuccessStatusCode) {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
