7f7c7ccc0ec79d48f8a606409d07c7274428205e
M-racle <108946729+M-racle@users.noreply.github.com>
2023-12-26 22:03:44 +0300

    EF

--- Api/Hubs/SignalRHub.cs
+++ Api/Hubs/SignalRHub.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Api.Hubs {
     public class SignalRHub : Hub{
+        Context context = new Context();
+        
+        public async Task SendCategoryCount() {
+            var value = context.Categories.Count();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value); // Burada gelen değeri client tarafına gönderme
 
+        }
     }
 }
--- WebUI/Controllers/SignalRDefaultController.cs
+++ WebUI/Controllers/SignalRDefaultController.cs
@@ -5,5 +5,9 @@ namespace WebUI.Controllers {
         public IActionResult Index() {
             return View();
         }
+
+        public IActionResult Index2() {
+            return View();
+        }
     }
 }
