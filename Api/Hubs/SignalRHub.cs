99e8a0547e2a917436e39d57ecf787954bee7331
M-racle <108946729+M-racle@users.noreply.github.com>
2024-03-10 18:43:31 +0300

    Moved the Active Category Number and Passive Category Number to the statistics page with SignalR

--- Api/Hubs/SignalRHub.cs
+++ Api/Hubs/SignalRHub.cs
@@ -12,14 +12,21 @@ namespace Api.Hubs {
             _productService = productService;
         }
 
-        public async Task SendCategoryCount() {  
+        public async Task SendStatistics() {  
             var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", value); // Burada gelen değeri client tarafına gönderme
-        }
 
-        public async Task SendProductCount() {
             var value2 = _productService.TCategoryCount(); // Normalde Product count olacaktı
             await Clients.All.SendAsync("ReceiveProductCount", value2); // Burada gelen değeri client tarafına gönderme
+
+            var value3 = _categoryService.TActiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategeryCount", value3);
+
+            var value4 = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("ReceivePasiveCategeryCount", value4);
+
+
         }
+
     }
 }
