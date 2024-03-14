13cbc3e526537f7885288396bd42121af7c7c750
M-racle <108946729+M-racle@users.noreply.github.com>
2024-03-14 11:51:32 +0300

    Separation of Layout into Component Partials for UI is completed

---
+++ WebUI/ViewComponents/UILayoutComponents/_UILAyoutScriptComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.UILayoutComponents {
+    public class _UILAyoutScriptComponentPartial : ViewComponent{
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
