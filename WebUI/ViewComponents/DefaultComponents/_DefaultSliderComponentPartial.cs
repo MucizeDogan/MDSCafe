d1746c96962c47553ad63a510760520afd7279d5
M-racle <108946729+M-racle@users.noreply.github.com>
2024-03-14 16:47:06 +0300

    Titles and Descriptions in the Slider on the Home Page have been adjusted to be dynamically retrieved from the database.

--- WebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ WebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,8 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.Dtos.SliderDto;
 
 namespace WebUI.ViewComponents.DefaultComponents {
     public class _DefaultSliderComponentPartial  : ViewComponent{
-        public IViewComponentResult Invoke() {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var client = _httpClientFactory.CreateClient(); // Bir istemci oluşturdum.
+            var res = await client.GetAsync("https://localhost:7052/api/Sliders"); // İstekte bulunacağımız apinin url sini yazıyoruz
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync(); // json dan gelen içerği string formatta oku
+                var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData); // Json datayı çözüp normal metine dönüştürür(DeserializeObject)
+                return View(values);
+            }
             return View();
         }
     }
