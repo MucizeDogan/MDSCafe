97a1371b82171194558287a4668fea6bf3b6e1a6
M-racle <108946729+M-racle@users.noreply.github.com>
2024-11-16 12:02:06 +0300

    The About Us and Testimonials sections on the home page have been placed dynamically according to the information in the database.

--- WebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
+++ WebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
@@ -1,8 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.Dtos.TestimonialDto;
 
 namespace WebUI.ViewComponents.DefaultComponents {
     public class _DefaultTestimonialComponentPartial : ViewComponent {
-        public IViewComponentResult Invoke() {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultTestimonialComponentPartial(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var client = _httpClientFactory.CreateClient(); // Bir istemci oluşturdum.
+            var res = await client.GetAsync("https://localhost:7052/api/Testimonial"); // İstekte bulunacağımız apinin url sini yazıyoruz
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync(); // json dan gelen içerği string formatta oku
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData); // Json datayı çözüp normal metine dönüştürür(DeserializeObject)
+                return View(values);
+            }
             return View();
         }
     }
