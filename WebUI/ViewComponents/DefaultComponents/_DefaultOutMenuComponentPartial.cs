f3ed16c991813cf7b00294a9cf4e384738e6e715
M-racle <108946729+M-racle@users.noreply.github.com>
2024-11-16 11:02:31 +0300

    In the Our Menu section on the home page, I have listed the products with true status, which are our featured products for now. In this section, I will revise and filter by categories later.

--- WebUI/ViewComponents/DefaultComponents/_DefaultOutMenuComponentPartial.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace WebUI.ViewComponents.DefaultComponents {
-    public class _DefaultOutMenuComponentPartial : ViewComponent{
-        public IViewComponentResult Invoke() {
-            return View();
-        }
-    }
-}
