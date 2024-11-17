8c18257c13c34e9563e455a81e80893399309ff5
M-racle <108946729+M-racle@users.noreply.github.com>
2024-11-17 12:20:45 +0300

    I used a method to get the id value of the selected product in the menu using ajax.

--- WebUI/Controllers/MenuController.cs
+++ WebUI/Controllers/MenuController.cs
@@ -1,8 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.Dtos.ProductDto;
 
 namespace WebUI.Controllers {
     public class MenuController : Controller {
-        public IActionResult Index() {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuController(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index() {
+            var client = _httpClientFactory.CreateClient(); // Bir istemci oluşturdum.
+            var res = await client.GetAsync("https://localhost:7052/api/Product"); // İstekte bulunacağımız apinin url sini yazıyoruz
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync(); // json dan gelen içerği string formatta oku
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData); // Json datayı çözüp normal metine dönüştürür(DeserializeObject)
+                return View(values);
+            }
             return View();
         }
     }
