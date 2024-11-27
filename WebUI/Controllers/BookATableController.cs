c42ca48fb7b68adfe81a5d424e65c49d024266e5
M-racle <108946729+M-racle@users.noreply.github.com>
2024-11-27 15:08:44 +0300

    The reservation page has been created. The reservation creation process has been completed successfully.
    The list of reservations created in the reservation section on the admin side is instantly printed to the console with SignalR.

---
+++ WebUI/Controllers/BookATableController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using WebUI.Dtos.BookingDto;
+
+namespace WebUI.Controllers {
+    public class BookATableController : Controller {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BookATableController(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateBookingDto createBookingDto) {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var res = await client.PostAsync("https://localhost:7052/api/Booking", stringContent);
+            if (res.IsSuccessStatusCode) {
+                ViewBag.SuccessMessage = $"Tarih: {createBookingDto.Date.ToShortDateString()}, Kişi Sayısı: {createBookingDto.PersonCount}";
+                return View();
+            }
+
+            ViewBag.ErrorMessage = "Lütfen tekrar deneyiniz.";
+            return View();
+        }
+    }
+}
