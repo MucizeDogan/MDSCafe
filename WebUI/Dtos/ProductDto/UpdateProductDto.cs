7db90f4fdf441ee2f8c89922eb244246a8eca0ce
M-racle <108946729+M-racle@users.noreply.github.com>
2024-12-10 11:00:38 +0300

    Revisions were made to the categories, products and sidebar pages.

--- WebUI/Dtos/ProductDto/UpdateProductDto.cs
+++ WebUI/Dtos/ProductDto/UpdateProductDto.cs
@@ -1,8 +1,11 @@
-﻿namespace WebUI.Dtos.ProductDto {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Dtos.ProductDto {
     public class UpdateProductDto {
         public int ProductID { get; set; }
         public string ProductName { get; set; }
         public string Description { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.##}", ApplyFormatInEditMode = true)]
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public bool ProductStatus { get; set; }
