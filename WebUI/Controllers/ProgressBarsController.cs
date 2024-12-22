a71ecfcd2412a75bb2e5b3fb2e524462d638f652
M-racle <108946729+M-racle@users.noreply.github.com>
2024-12-22 11:15:32 +0300

    ProgressBar operations have been performed.

--- WebUI/Controllers/ProgressBarsController.cs
+++ WebUI/Controllers/ProgressBarsController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Controllers {
     public class ProgressBarsController : Controller {
+        [AllowAnonymous]
         public IActionResult Index() {
             return View();
         }
