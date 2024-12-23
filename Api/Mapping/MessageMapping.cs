3282d8e26cadc32d95dd2a63eeb34485d7843410
M-racle <108946729+M-racle@users.noreply.github.com>
2024-12-23 17:44:31 +0300

    The problem of not being able to send a message in the Contact Us section has been resolved. The map section has been made to show the desired location.

---
+++ Api/Mapping/MessageMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DtoLayer.MessageDto;
+using EntityLayer.Entities;
+
+namespace Api.Mapping {
+    public class MessageMapping : Profile {
+        public MessageMapping()
+        {
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, GetMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+        }
+    }
+}
