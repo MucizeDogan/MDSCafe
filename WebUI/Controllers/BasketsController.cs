520a0b2b45112eeb97d9585c72aba58f610646a0
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-07 10:52:07 +0300

    The process of bringing the selected orders according to the selected table was done. If the table is full, the user can see their basket or add products to their basket from the menu.

--- WebUI/Controllers/BasketsController.cs
+++ WebUI/Controllers/BasketsController.cs
@@ -12,9 +12,10 @@ namespace WebUI.Controllers {
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IActionResult> Index() {
+        public async Task<IActionResult> Index(int id) {
+            TempData["x"] = id;
             var client = _httpClientFactory.CreateClient(); // Bir istemci oluşturdum.
-            var res = await client.GetAsync("https://localhost:7052/api/Basket/BasketListByCafeTableWithProductName?id=1"); // İstekte bulunacağımız apinin url sini yazıyoruz
+            var res = await client.GetAsync("https://localhost:7052/api/Basket/BasketListByCafeTableWithProductName?id=" + id); // İstekte bulunacağımız apinin url sini yazıyoruz
             if (res.IsSuccessStatusCode) {
                 var jsonData = await res.Content.ReadAsStringAsync(); // json dan gelen içerği string formatta oku
                 var values = JsonConvert.DeserializeObject<List<ResultBasketDto>>(jsonData); // Json datayı çözüp normal metine dönüştürür(DeserializeObject)
@@ -24,10 +25,11 @@ namespace WebUI.Controllers {
         }
 
         public async Task<IActionResult> DeleteBasket(int id) {
+            int tableId = int.Parse(TempData["x"].ToString());
             var client = _httpClientFactory.CreateClient();
             var res = await client.DeleteAsync($"https://localhost:7052/api/Basket/{id}");
             if (res.IsSuccessStatusCode) {
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new {id = tableId });
             }
             return NoContent();
         }
